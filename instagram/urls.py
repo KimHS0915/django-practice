196d6348af37b9fa511c2c6421d6f1754f2a964f
KimHS0915 <remember2552@gmail.com>
2020-09-05 23:39:14 +0900

    TemplateView, RedirectView

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,7 +18,7 @@ register_converter(YearConverter, 'year')
 app_name = 'instagram'
 
 urlpatterns = [
-	path('', views.post_list),
+	path('', views.post_list, name='post_list'),
 	path('<int:pk>/', views.post_detail),
 	# path('archives/<int:year>/', views.archives_year),
 	# re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
