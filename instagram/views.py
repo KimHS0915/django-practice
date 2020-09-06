a880ff0f298cca6d08ffce07ce4fb81d90be385b
KimHS0915 <remember2552@gmail.com>
2020-09-06 19:40:36 +0900

    Django CBV - ListView, DetailView

--- instagram/views.py
+++ instagram/views.py
@@ -14,7 +14,7 @@ from .models import Post
 # 		'q': q,
 # 	})
 
-post_list = ListView.as_view(model=Post)
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
 
 # def post_detail(request, pk):
@@ -27,7 +27,24 @@ post_list = ListView.as_view(model=Post)
 # 		'post': post,
 # 	})
 
-post_detail = DetailView.as_view(model=Post)
+# post_detail = DetailView.as_view(
+# 	model=Post,
+# 	queryset=Post.objects.filter(is_public=True)
+# 	)
+
+
+class PostDetailView(DetailView):
+	model = Post
+	# queryset = Post.objects.filter(is_public=True)
+
+	def get_queryset(self):
+		qs = super().get_queryset()
+		if not self.request.user.is_authenticated:
+			qs = qs.filter(is_public=True)
+		return qs
+
+
+post_detail = PostDetailView.as_view()
 
 
 def archives_year(request, year):
